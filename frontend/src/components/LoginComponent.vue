3ad7b4b88cd647f906a597ab0d829e95b4283bd7
kimsungsu <zxzxzlxlzlxl@naver.com>
2023-11-13 21:47:28 +0900

    refactor : nav bar 디자인 수정

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -1,79 +1,61 @@
-//
-<script setup>
-// import { reactive } from "vue";
-// const formState = reactive({
-//   username: "",
-//   password: "",
-//   //   remember: true, // true일 떄 쿠키 등록해주는 방식 쓰면 아이디 저장될 듯?
-// });
-// const onFinish = (values) => {
-//   console.log("Success:", values);
-// };
-// const onFinishFailed = (errorInfo) => {
-//   console.log("Failed:", errorInfo);
-// };
-//
-</script>
+<script setup></script>
 
-//
 <template>
-  //
   <div class="background">
-    //
     <a-form class="login-form">
-      //
-      <div>// <img src="@\assets\img\PlanIt!.png" alt="logo" /> //</div>
+      <div><img src="@\assets\img\PlanIt!.png" alt="logo" /></div>
 
-      // <a-label for="id">아이디 </a-label> // <a-input id="id" name="id" type="text"> </a-input>
+      <a-label for="id">아이디 </a-label>
+      <a-input id="id" name="id" type="text"> </a-input>
 
-      // <a-label for="id">패스워드 </a-label> // <a-input id="id" name="id" type="text"> </a-input>
+      <a-label for="id">패스워드 </a-label>
+      <a-input id="id" name="id" type="text"> </a-input>
 
-      // <a-button type="primary" htmlType="submit">로그인</a-button> //
+      <a-button type="primary" htmlType="submit">로그인</a-button>
       <!-- <p>아직 회원이 아니세요?<a href="/users/regist">회원가입</a></p> -->
-      //
-      <p>아직 회원이 아니세요?<router-link to="/users/regist">회원가입</router-link></p>
-      //
+
+      <p>
+        아직 회원이 아니세요?<router-link to="/users/regist"
+          >회원가입</router-link
+        >
+      </p>
     </a-form>
-    //
   </div>
-  //
 </template>
 
-//
 <style lang="scss" scoped>
-// .login-form {
-//   width: 27rem;
-//   height: 30rem;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center; /* Center vertically */
-//   align-items: center; /* Center horizontally */
-//   margin: 10rem auto; /* Center within the parent container */
-//   border: 1px solid #20b2aa;
-//   border-radius: 2rem;
+.login-form {
+  width: 27rem;
+  height: 30rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* Center vertically */
+  align-items: center; /* Center horizontally */
+  margin: 10rem auto; /* Center within the parent container */
+  border: 1px solid #20b2aa;
+  border-radius: 2rem;
 
-//   a-label {
-//     margin-top: 1rem;
-//     text-align: left;
-//     color: gray;
-//   }
+  a-label {
+    margin-top: 1rem;
+    text-align: left;
+    color: gray;
+  }
 
-//   button {
-//     margin-top: 1rem;
+  button {
+    margin-top: 1rem;
 
-//     border-radius: 2rem;
-//     width: 20rem;
-//     height: 2rem;
-//   }
+    border-radius: 2rem;
+    width: 20rem;
+    height: 2rem;
+  }
 
-//   img {
-//     width: 12rem;
-//     height: 12rem;
-//   }
+  img {
+    width: 12rem;
+    height: 12rem;
+  }
 
-//   p {
-//     color: gray;
-//   }
-// }
-//
+  p {
+    color: gray;
+  }
+}
 </style>
--- frontend/src/components/layout/TheHeadingNavBar.vue
+++ frontend/src/components/layout/TheHeadingNavBar.vue
@@ -1,40 +1,42 @@
+<script></script>
 <template>
-  <a-layout>
-    <a-layout-header class="header" style="display: flex; justify-content: flex-end">
-      <div class="logo">
-        <!-- <img src="@\assets\img\PlanIt!.png" alt="logo" /> -->
-      </div>
-      <a-menu
-        v-model:selectedKeys="selectedKeys1"
-        theme="dark"
-        mode="horizontal"
-        :style="{ lineHeight: '64px', display: 'flex', alignItems: 'center', marginLeft: 'auto' }"
+  <nav>
+    <a-layout>
+      <a-layout-header
+        class="header"
+        style="display: flex; justify-content: flex-end"
       >
-        <a-menu-item key="1"><router-link to="/users/login">로그인</router-link></a-menu-item>
-        <a-menu-item key="2"><router-link to="/users/regist">회원가입</router-link></a-menu-item>
-      </a-menu>
-    </a-layout-header>
-    <!-- <a-layout-footer style="text-align: center">
+        <a href="/"><img src="@\assets\img\PlanIt!.png" alt="logo" /></a>
+        <a-menu
+          v-model:selectedKeys="selectedKeys1"
+          mode="horizontal"
+          :style="{
+            lineHeight: '64px',
+            display: 'flex',
+            alignItems: 'center',
+            marginLeft: 'auto',
+          }"
+        >
+          <a-menu-item key="1"
+            ><router-link to="/users/login">로그인</router-link></a-menu-item
+          >
+          <a-menu-item key="2"
+            ><router-link to="/users/regist">회원가입</router-link></a-menu-item
+          >
+        </a-menu>
+      </a-layout-header>
+      <!-- <a-layout-footer style="text-align: center">
       Ant Design ©2018 Created by Ant UED
     </a-layout-footer> -->
-  </a-layout>
+    </a-layout>
+  </nav>
 </template>
-<script></script>
-<style scoped>
-/* #components-layout-demo-top-side .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
+<style lang="scss" scoped>
+.header {
+  background-color: white;
+  img {
+    width: 10rem;
+    height: 4rem;
+  }
 }
-
-.ant-row-rtl #components-layout-demo-top-side .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-} */
 </style>
