17dc9605262fc165f102331431a27c311b2e8fb4
zxzxzlxlzlxl <zxzxzlxlzlxl@naver.com>
2023-11-13 12:57:50 +0900

    refactor : swagger 파일 충돌 해결

--- backend/src/main/java/com/trip/planit/config/SwaggerConfiguration.java
+++ backend/src/main/java/com/trip/planit/config/SwaggerConfiguration.java
@@ -24,41 +24,38 @@ public class SwaggerConfiguration {
 	 * version ?, title ?
 	 * version 및 title: API의 버전 및 제목을 정의하는 문자열 변수입니다.
 	 * http://localhost/swagger-ui/index.html#
-	 * 	 
-	*/
+	 * 
+	 */
 	private String version = "V1";
 	private String title = "plan API : " + version;
-	
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).consumes(getConsumeContentTypes()).produces(getProduceContentTypes())
-					.apiInfo(apiInfo()).groupName(version).select()
-<<<<<<< HEAD
-					.apis(RequestHandlerSelectors.basePackage("com.trip.planit.user.controller"))
-					.paths(regex("/users/.*")).build()
-=======
-					.apis(RequestHandlerSelectors.basePackage("com.trip.planit.plan.controller"))
-					.paths(regex("/plans/.*")).build()
->>>>>>> origin/feature#4
-					.useDefaultResponseMessages(false);
+		return new Docket(DocumentationType.SWAGGER_2).consumes(getConsumeContentTypes())
+				.produces(getProduceContentTypes())
+				.apiInfo(apiInfo()).groupName(version).select()
+				.apis(RequestHandlerSelectors.basePackage("com.trip.planit.user.controller"))
+				.paths(regex("/users/.*")).build()
+				.useDefaultResponseMessages(false);
 	}
-	
-	private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
-    }
 
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
-    }
+	private Set<String> getConsumeContentTypes() {
+		Set<String> consumes = new HashSet<>();
+		consumes.add("application/json;charset=UTF-8");
+		consumes.add("application/x-www-form-urlencoded");
+		return consumes;
+	}
+
+	private Set<String> getProduceContentTypes() {
+		Set<String> produces = new HashSet<>();
+		produces.add("application/json;charset=UTF-8");
+		return produces;
+	}
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title(title)
-				.description("<h3>SSAFY API Reference for Developers</h3>Swagger를 이용한 Board API<br><img src=\"/assets/img/ssafy_logo.png\" width=\"150\">") 
+				.description(
+						"<h3>SSAFY API Reference for Developers</h3>Swagger를 이용한 Board API<br><img src=\"/assets/img/ssafy_logo.png\" width=\"150\">")
 				.contact(new Contact("SSAFY", "https://edu.ssafy.com", "ssafy@ssafy.com"))
 				.license("SSAFY License")
 				.licenseUrl("https://www.ssafy.com/ksp/jsp/swp/etc/swpPrivacy.jsp")
