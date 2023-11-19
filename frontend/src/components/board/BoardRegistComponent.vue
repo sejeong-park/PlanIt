27a225f7d757803c1087519cf1213ab4962a4a4b
kimsungsu <zxzxzlxlzlxl@naver.com>
2023-11-19 17:59:02 +0900

    feat : 썸네일 등록 모달 창 추가

--- frontend/src/components/board/BoardRegistComponent.vue
+++ frontend/src/components/board/BoardRegistComponent.vue
@@ -16,24 +16,29 @@ const updateContent = (event) => {
 //   }
 // );
 // console.log("title : ", title.value);
+
+const isFileModalOpen = ref(false);
+
+const openFileModal = () => {
+  isFileModalOpen.value = true;
+};
+
+const closeFileModal = () => {
+  isFileModalOpen.value = false;
+};
 </script>
 
 <template>
   <div class="board-editor">
     <div class="post-title">
-      <div class="title-input-container">
-        <textarea
-          id="post-title-inp"
-          class="textarea_tit"
-          placeholder="제목을 입력하세요"
-          style="height: 42px !important"
-          v-model="title"
-        ></textarea>
-        <div class="file-input-container">
-          <input type="file" />
-        </div>
-        <div id="board-editor-container" style="display: block"></div>
-      </div>
+      <textarea
+        id="post-title-inp"
+        class="textarea_tit"
+        placeholder="제목을 입력하세요"
+        style="height: 42px !important"
+        v-model="title"
+      ></textarea>
+      <div id="board-editor-container" style="display: block"></div>
     </div>
     <body
       id="tinymce"
@@ -55,22 +60,73 @@ const updateContent = (event) => {
     >
       <p data-ke-size="size16"><br /></p>
     </body>
+    <div class="content-aside">
+      <div class="wrap_btn">
+        <button
+          id="publish-layer-btn"
+          class="btn btn-default"
+          @click="openFileModal"
+        >
+          완료
+        </button>
+      </div>
+    </div>
+    <div v-if="isFileModalOpen" class="file-modal">
+      <div class="sc-jHkVzv hrosqC">
+        <div class="sc-bQtKYq fZgivC">
+          <div class="sc-fXEqDS eUikOV">
+            <button
+              class="sc-jWUzzU hhwHgR"
+              style="
+                background-color: #4caf50;
+                color: white;
+                padding: 10px 20px;
+                border: none;
+                cursor: pointer;
+                border-radius: 4px;
+              "
+            >
+              썸네일 업로드
+            </button>
+          </div>
+        </div>
+      </div>
+      <button
+        @click="closeFileModal"
+        style="
+          background-color: #d9534f;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          cursor: pointer;
+          border-radius: 4px;
+        "
+      >
+        취소
+      </button>
+      <button
+        class="sc-jWUzzU hhwHgR"
+        style="
+          background-color: gray;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          cursor: pointer;
+          border-radius: 4px;
+        "
+      >
+        출간하기
+      </button>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .post-title {
-  display: flex;
   width: 860px;
   margin: 35px auto 17px;
   padding-bottom: 26px;
   border-bottom: 1px solid rgba(0, 0, 0, 0.05);
-
-  .title-input-container,
-  .file-input-container {
-    flex: 1; /* 자식들이 동일한 너비를 가지도록 설정 */
-    margin-right: 10px;
-  }
 
   .textarea_tit {
     display: block;
@@ -107,9 +163,60 @@ const updateContent = (event) => {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   letter-spacing: 0;
+  // position: relative;
 }
 body {
   display: block;
   margin: 8px;
+}
+
+.content-aside {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 66px;
+  background-color: #f5f5f5;
+  z-index: 1;
+  overflow: hidden;
+  min-width: 944px;
+  text-align: right;
+  position: fixed; // 완료 버튼이 아래 고정
+  bottom: 0;
+  left: 0;
+  text-align: center;
+
+  .wrap_btn {
+    margin: 10px;
+  }
+
+  .btn {
+    --tw-bg-opacity: 1;
+    /* 배경색 설정 */
+    background-color: rgb(80 212 229 / var(--tw-bg-opacity));
+    color: white; /* 글자색 설정 */
+    padding: 12px 24px; /* 안쪽 여백 설정 */
+    font-size: 16px; /* 글자 크기 설정 */
+    border: none; /* 테두리 없애기 */
+    border-radius: 4px; /* 테두리 모서리 둥글게 만들기 */
+    cursor: pointer; /* 커서 모양 변경 */
+    transition: opacity 0.3s ease; /*투명도 전환을 부드럽게 만들기 위한 트랜지션 속성 */
+    font-weight: 700;
+    width: 10rem;
+  }
+
+  .btn:hover {
+    opacity: 0.6; /*마우스 호버 시 투명도를 0.8로 조정 */
+  }
+}
+
+.file-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border: 1px solid #ccc;
+  z-index: 2;
 }
 </style>
